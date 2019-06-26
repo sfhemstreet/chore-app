43c9be61e72a7dbd909098f7d1fe727871732762
sfhemstreet <sfhemstreet@gmail.com>
2019-06-26 15:19:51 -0700

    added router

---
+++ src/components/Home/GuestHome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class GuestHome extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    render(){
+        return (
+            <div>
+                Guest Home
+            </div>
+        )
+    }
+
+
+}
+
+export default GuestHome;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import Tilt from 'react-tilt';
 import './Logo.css';
 import logo from './logo.png';
 
 const Logo = ({onRouteChange}) => {
     return(
-        <div 
-            className="db dtc-l v-mid mid-gray link w-100 w-25-l tc tl-l mb2 mb0-l">
+        <div className="db dtc-l v-mid mid-gray link w-100 w-25-l tc tl-l mb2 mb0-l">
             <Tilt 
                 className="Tilt shadow-2  br-100 dib w2 h2 pointer" 
                 options={{ max : 55 }} 
                 style={{ height: 100, width: 100 }} >
+                <NavLink to='/' >
                 <img onClick={() => onRouteChange('home')} className=" br-100"  alt="UChore" src={logo}/> 
+                </ NavLink>
             </Tilt>
         </div>
     )
