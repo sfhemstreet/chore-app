348f1fb2025eb1eb289dbe9b5d0afa6868d8d522
sfhemstreet <sfhemstreet@gmail.com>
2019-10-24 14:14:31 -0700

    register redirect

--- src/components/Register/RegisterSuccess.js
+++ src/components/Register/RegisterSuccess.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-const RegisterSuccess = ({email, name, history}) => {
-    setTimeout(() => history.push('/signin'), 20000);
-    
-    const msg = name ? `{name}, you are registered! Just one more step. <br/> A confirmation email has been sent to {email}. Please click on the confirmation link in the email to activate your Chore account.`
-                        : `Seems you have already filled out an submitted a registration form. Not you? Refresh the page!`;
+const RegisterSuccess = ({email, name, exit}) => {
+    setTimeout(() => exit(), 20000);
     
     return (
         <div className='tc pa2'> 
             <fieldset className="ba b--transparent ph0 mh0">
                 <div className='pa2 center'>
-                    {msg}
+                {name}, you are registered! Just one more step. <br/> A confirmation email has been sent to {email}. Please click on the confirmation link in the email to activate your Chore account.
                 </div>
             </fieldset>
         </div>
